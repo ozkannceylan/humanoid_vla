f858e77ca2a4031eadb89bf64f554c4ac1cc6302
ozkannceylan <ozkannceylan@gmail.com>
2026-02-26 12:00:46 +0100

    Add .gitignore and remove build/venv artifacts from tracking

--- ros2_ws/build/vla_mujoco_bridge/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/media/orka/storage/robotics/ros2_ws/install/vla_mujoco_bridge'
--- ros2_ws/build/vla_mujoco_bridge/share/vla_mujoco_bridge/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/media/orka/storage/robotics/ros2_ws/build/vla_mujoco_bridge"
